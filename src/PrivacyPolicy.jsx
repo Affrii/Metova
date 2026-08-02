15c2cba313b380d51efd8720ed819cf8f3f17013
Afra <salmajaffar29@gmail.com>
2026-08-02 15:41:12 +0530

    Debug privacy policy close

--- src/PrivacyPolicy.jsx
+++ src/PrivacyPolicy.jsx
@@ -24,7 +24,11 @@ function PrivacyPolicy({ onClose }) {
         }}>
           Privacy Policy
         </h1>
-        <div onClick={(e) => { e.stopPropagation(); onClose(); }} style={{
+        <div onClick={(e) => { 
+  e.stopPropagation()
+  console.log("X clicked!")
+  onClose()
+}} style={{
           width: "36px", height: "36px", borderRadius: "50%",
           backgroundColor: "#FDF0EC", border: "0.5px solid #E8E4F0",
           display: "flex", alignItems: "center", justifyContent: "center",
